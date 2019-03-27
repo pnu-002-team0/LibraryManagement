d3f61a058e66a41f2a6b571877b7d52e41b2ada4
NightOwl <lhh80482@gmail.com>
2019-03-27 15:28:45 +0900

    junit4 add

--- src/test/BookManagementTest2.java
+++ src/test/BookManagementTest2.java
@@ -1,22 +1,22 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import main.BookManagement;
 
-public class BookManagementTest {
+public class BookManagementTest2 {
 
 	@Test
-	public void testAddBook() {
+	void testAddBook() {
 		BookManagement book_management = new BookManagement();
 		assertTrue(book_management.addBook(2));
 		assertFalse(book_management.addBook(-1));
 	}
 
 	@Test
-	public void testGetBookCount() {
+	void testGetBookCount() {
 		BookManagement book_management = new BookManagement();
 		assertTrue(book_management.getBookCount()==0);
 	}
