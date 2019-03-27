db8d183943369ef3c917b747b9ed32b01877ee3f
NightOwl <lhh80482@gmail.com>
2019-03-27 15:51:26 +0900

    test add

--- src/test/BookManagementTest.java
+++ src/test/BookManagementTest.java
@@ -12,8 +12,13 @@ public class BookManagementTest {
 	public void testAddBook() {
 		BookManagement book_management = new BookManagement();
 		assertTrue(book_management.addBook(2));
+	}
+	@Test
+	public void testAddBook_False() {
+		BookManagement book_management = new BookManagement();
 		assertFalse(book_management.addBook(-1));
 	}
+
 
 	@Test
 	public void testGetBookCount() {
